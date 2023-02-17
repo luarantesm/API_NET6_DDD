c1c55486bed52d1e9f5a7e9b91dd07ecf4cd7b50
Luciano Arantes Moncao <luciano.moncao@tellussa.onmicrosoft.com>
2023-02-17 17:29:13 -0300

    Ajustes JWT e Program

---
+++ WebAPIS/Program.cs
@@ -0,0 +1,106 @@
+using AutoMapper;
+using Domain.Interfaces;
+using Domain.Interfaces.Generics;
+using Domain.Interfaces.InterfacesServices;
+using Domain.Services;
+using Entites.Entities;
+using Infrastructure.Configuration;
+using Infrastructure.Repository.Generics;
+using Infrastructure.Repository.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using WebAPIS.Models;
+using WebAPIS.Token;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//ConfigServices
+builder.Services.AddDbContext<ContextBase>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ContextBase>();
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages();
+
+//Interface e Repositorio - Injeção de Dependencia
+builder.Services.AddSingleton(typeof(IGeneric<>), typeof(RepositoryGenerics<>));
+builder.Services.AddSingleton<IMessage, RepositoryMessage>();
+
+//Serviço Domonio
+builder.Services.AddSingleton<IServiceMessage, ServiceMessage>();
+
+//JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(option =>
+{
+    option.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = false,
+
+        ValidIssuer = "Teste.Security.Bearer",
+        ValidAudience = "Teste.Security.Bearer",
+        IssuerSigningKey = JWTSecurityKey.Create("Secret_Key-12345678")
+    };
+
+    option.Events = new JwtBearerEvents
+    {
+        OnAuthenticationFailed = context =>
+        {
+            Console.WriteLine("OnAuthenticaitonFailed: " + context.Exception.Message);
+            return Task.CompletedTask;
+        },
+        OnTokenValidated = context =>
+        {
+            Console.WriteLine("OnTokerValidated: " + context.SecurityToken);
+            return Task.CompletedTask;
+        }
+    };
+});
+
+var config = new AutoMapper.MapperConfiguration(cfg =>
+{
+    cfg.CreateMap<MessageViewModel, Message>();
+    cfg.CreateMap<Message, MessageViewModel>();
+});
+
+IMapper mapper = config.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var devCliente = "http://localhost:4200";
+
+app.UseCors(x => x
+.AllowAnyOrigin()
+.AllowAnyMethod()
+.AllowAnyHeader()
+.WithOrigins(devCliente));
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.UseSwaggerUI();
+
+app.Run();
---
+++ WebAPIS/Token/TokenJWTBuilder.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace WebAPIS.Token
+{
+    public class TokenJWTBuilder
+    {
+        private SecurityKey secretKey = null;
+        private string subject = "";
+        private string issuer = "";
+        private string audience = "";
+        private Dictionary<string, string> claims = new Dictionary<string, string>();
+        private int expiryInMinutes = 5;
+
+        public TokenJWTBuilder AddSecurityKey(SecurityKey securityKey)
+        {
+            this.secretKey = securityKey;
+            return this;
+        }
+
+        public TokenJWTBuilder AddSubject(string subject)
+        {
+            this.subject = subject;
+            return this;
+        }
+
+        public TokenJWTBuilder AddIssuert(string issuer)
+        {
+            this.issuer = issuer;
+            return this;
+        }
+
+        public TokenJWTBuilder AddAudience(string audience)
+        {
+            this.audience = audience;
+            return this;
+        }
+
+        public TokenJWTBuilder AddClaim(string type, string value)
+        {
+            this.claims.Add(type, value);
+            return this;
+        }
+
+        public TokenJWTBuilder AddExpity(int expiryInMinutes)
+        {
+            this.expiryInMinutes = expiryInMinutes;
+            return this;
+        }
+
+        public void EnsureArguments()
+        {
+            if (this.secretKey == null)
+                throw new ArgumentNullException("Security Key");
+
+            if (string.IsNullOrEmpty(this.subject))
+                throw new ArgumentNullException("Subject");
+
+            if (string.IsNullOrEmpty(this.issuer))
+                throw new ArgumentNullException("Issuer");
+
+            if (string.IsNullOrEmpty(this.audience))
+                throw new ArgumentNullException("Audience");
+        }
+
+        public TokenJWT Builder()
+        {
+            EnsureArguments();
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, this.subject),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            }.Union(this.claims.Select(item => new Claim(item.Key, item.Value)));
+
+            var token = new JwtSecurityToken(
+                issuer: this.issuer,
+                audience: this.audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(expiryInMinutes),
+                signingCredentials: new SigningCredentials(
+                    this.secretKey,
+                    SecurityAlgorithms.HmacSha256)
+                );
+
+            return new TokenJWT(token);
+        }
+    }
+}
