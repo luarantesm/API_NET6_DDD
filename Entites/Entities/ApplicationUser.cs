11c188dd71c81c52b53728f77e175a9541f07213
Luciano Arantes Moncao <luciano.moncao@tellussa.onmicrosoft.com>
2023-02-17 11:44:40 -0300

    Criação Entidades

---
+++ Entites/Entities/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Entites.Enums;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entites.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Column("USR_CPF")]
+        public string CPF { get; set; }
+
+        [Column("USR_TIPO")]
+        public TipoUsuario? Tipo { get; set; }
+    }
+}
---
+++ Entites/Entities/Notifies.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entites.Entities
+{
+    public class Notifies
+    {
+        public Notifies()
+        {
+            Notificacoes = new List<Notifies>();
+        }
+
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+
+        [NotMapped]
+        public string Mensagem { get; set; }
+
+        [NotMapped]
+        public List<Notifies> Notificacoes { get; set; }
+
+        public bool ValidarPropriedadeString(string valor, string nomePropriedade)
+        {
+            if (string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notificacoes.Add(new Notifies()
+                {
+                    Mensagem = "Campo obrigatório!",
+                    NomePropriedade = nomePropriedade
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidarPropriedadeInt(int valor, string nomePropriedade)
+        {
+            if (valor < 1 || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notificacoes.Add(new Notifies()
+                {
+                    Mensagem = "Campo obrigatório!",
+                    NomePropriedade = nomePropriedade
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
