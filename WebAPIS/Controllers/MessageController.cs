8653a43a9b6049eec7e4686cbd8af97574e35079
Luciano Arantes Moncao <luciano.moncao@tellussa.onmicrosoft.com>
2023-02-18 12:26:51 -0300

    Controller Message

---
+++ WebAPIS/Controllers/MessageController.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using Domain.Interfaces;
+using Entites.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIS.Models;
+
+namespace WebAPIS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IMessage _message;
+
+        public MessageController(IMapper mapper, IMessage message)
+        {
+            _mapper = mapper;
+            _message = message;
+        }
+
+        [Authorize]
+        [Produces("application/json")]
+        [HttpGet("/api/GetEntityById")]
+        public async Task<MessageViewModel> GetEntityById(Message message)
+        {
+            message = await _message.GetEntityById(message.Id);
+            var messageMap = _mapper.Map<MessageViewModel>(message);
+
+            return messageMap;
+        }
+
+        [Authorize]
+        [Produces("application/json")]
+        [HttpGet("/api/List")]
+        public async Task<List<MessageViewModel>> List()
+        {
+            var messages = await _message.List();
+            var messagesMap = _mapper.Map<List<MessageViewModel>>(messages);
+
+            return messagesMap;
+        }
+
+        [Authorize]
+        [Produces("application/json")]
+        [HttpPost("/api/Add")]
+        public async Task<List<Notifies>> Add(MessageViewModel message)
+        {
+            message.UserId = await RetornarIdUsuarioLogado();
+
+            var messageMap = _mapper.Map<Message>(message);
+            await _message.Add(messageMap);
+
+            return messageMap.Notificacoes;
+        }
+
+        [Authorize]
+        [Produces("application/json")]
+        [HttpPut("/api/Update")]
+        public async Task<List<Notifies>> Update(MessageViewModel message)
+        {
+            var messageMap = _mapper.Map<Message>(message);
+            await _message.Update(messageMap);
+
+            return messageMap.Notificacoes;
+        }
+
+        [Authorize]
+        [Produces("application/json")]
+        [HttpDelete("/api/Delete")]
+        public async Task<List<Notifies>> Delete(MessageViewModel message)
+        {
+            var messageMap = _mapper.Map<Message>(message);
+            await _message.Delete(messageMap);
+
+            return messageMap.Notificacoes;
+        }
+
+        private async Task<string> RetornarIdUsuarioLogado()
+        {
+            if (User != null)
+            {
+                var idUsuario = User.FindFirst("idUsuario");
+                return idUsuario.Value;
+            }
+
+            return string.Empty;
+        }
+    }
+}
--- WebAPIS/Program.cs
+++ WebAPIS/Program.cs
@@ -10,6 +10,7 @@ using Infrastructure.Repository.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using WebAPIS.Models;
 using WebAPIS.Token;
 
@@ -30,6 +31,38 @@ builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.R
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddRazorPages();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API_DDD_2022", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description =
+            "JWT Authorization Header - utilizado com Bearer Authentication.\r\n\r\n" +
+            "Digite 'Bearer' [espaço] e então seu token no campo abaixo.\r\n\r\n" +
+            "Exemplo (informar sem as aspas): 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+});
 
 //Interface e Repositorio - Injeção de Dependencia
 builder.Services.AddSingleton(typeof(IGeneric<>), typeof(RepositoryGenerics<>));
